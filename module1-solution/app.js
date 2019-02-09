575fe591090b3788ef5e4bef1acc73dd10315c7c
WilsonLu102 <WilsonLu08@gmail.com>
2019-02-09 18:46:53 -0500

    updated app.js with notes

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,33 +1,33 @@
-(function (){
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController ($scope){
-  $scope.ateTooMuchResponse="Please enter data first";
-  $scope.userInput="";
+  LunchCheckController.$inject = ['$scope'];
 
-  function calculateNumberOfCommasFromText(string){
-    var numberOfCommas = 0;
-    for(var i=0; i<string.length; i++){
-      if (string[i] == ',') {
+  function LunchCheckController($scope) {
+    $scope.ateTooMuchResponse = "Please enter data first";
+    $scope.userInput = "";
+
+    //After rereading the instructions, I realized I could have used the split function here, but I am leaving it as is the assignment does not require the usage of split to calculate the number of items
+    function calculateNumberOfCommasFromText(string) {
+      var numberOfCommas = 0;
+      for (var i = 0; i < string.length; i++) {
+        if (string[i] == ',') {
           numberOfCommas++;
+        }
       }
-    }
-    return numberOfCommas;
-  };
+      return numberOfCommas;
+    };
 
-  $scope.checkFoodList = function () {
-    var numberOfCommas = calculateNumberOfCommasFromText($scope.userInput);
-    if ($scope.userInput.length == 0) {
-      $scope.ateTooMuchResponse = "Please enter your data first";
-    }
-    else if (numberOfCommas > 2) {
-      $scope.ateTooMuchResponse = "Too much!";
-    }
-    else $scope.ateTooMuchResponse="Enjoy!"
-  };
-}
+    $scope.checkFoodList = function() {
+      var numberOfCommas = calculateNumberOfCommasFromText($scope.userInput);
+      if ($scope.userInput.length == 0) {
+        $scope.ateTooMuchResponse = "Please enter your data first";
+      } else if (numberOfCommas > 2) {
+        $scope.ateTooMuchResponse = "Too much!";
+      } else $scope.ateTooMuchResponse = "Enjoy!"
+    };
+  }
 })();
